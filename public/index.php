3d9bd3ae38a552537e113e2e70b0f1f12fce27c8
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-09-20 20:13:44 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+    require_once '../app/cores/App.php';
+    require_once '../app/cores/Controller.php';
+    require_once '../app/cores/Database.php';
+    require_once '../config/config.php';
+    require_once '../config/setup.php';
+    
+    $app = new App();
