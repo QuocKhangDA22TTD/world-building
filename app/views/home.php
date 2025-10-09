7300d62f88e8461cadb85d97de9713f764fdd1d0
TUANKHOA05052004 <nnguyendinhtuankhoatt209@.com>
2025-10-09 08:58:28 +0700

    upadate file home.php

--- app/views/home.php
+++ app/views/home.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . './header.php';
-include_once __DIR__ . './navigation.php';
-include_once __DIR__ . './content.php';
-include_once __DIR__ . './footer.php';
+include_once __DIR__ . '/header.php';
+include_once __DIR__ . '/navigation.php';
+include_once __DIR__ . '/content.php';
+include_once __DIR__ . '/footer.php';
