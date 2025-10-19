c473634a6bc8d4dcb9b0c732806c388b6967230d
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-10-19 15:07:18 +0700

    update file create_schema.sql

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(150) UNIQUE
+    email VARCHAR(150) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Bảng thế giới
