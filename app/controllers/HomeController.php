868083f930d2688b27126099019e8b051efaafc0
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-01-01 21:24:16 +0700

    reset dự án

--- app/controllers/HomeController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class HomeController extends Controller {
-    public function index() {
-        $data = ['title' => 'Trang Chủ', 'message' => 'Chào mừng bạn đến với MVC PHP thuần!'];
-        $this->view('home', $data);
-    }
-}
